56ca41f24c13a2f9c2ee2d574611516a6b67b2b0
Keshav18khandelwal <khandelwal18keshav@gmail.com>
2022-02-07 14:24:30 +0530

    some updates

--- Backend/frontend/src/component/Login.js
+++ Backend/frontend/src/component/Login.js
@@ -25,7 +25,7 @@ export default function Login(props) {
     const [credential, setcredential] = useState({ email: "", password: "" });
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch(`/api/auth/login`, {
+        const response = await fetch(`http://localhost:5000/api/auth/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
