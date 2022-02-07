de3d944336332aac7cac861c2767b65ed1ab5f6d
Keshav18khandelwal <khandelwal18keshav@gmail.com>
2022-02-07 17:41:29 +0530

    some updates

--- Backend/index.js
+++ Backend/index.js
@@ -17,9 +17,9 @@ app.use('/api/notes', require('./routes/notes'));
 //for heroku 
 if(process.env.NODE_ENV=="production"){
   app.use(express.static("frontend/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  // });
 }
 
 app.listen(port, () => {
